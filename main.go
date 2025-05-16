f4b01cec932b9d3e42b2a0fcf952df3c8473eb8f
Durved <durved96@gmail.com>
2025-05-16 14:15:44 +0300

    Add function

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+func F(a, b, x float64) float64 {
+	numerator := math.Pow(b, 3) + math.Pow(math.Sin(a*x), 2)
+	denominator := math.Acos(x*b*x) + math.Exp(-x/2)
+	return numerator / denominator
 }
